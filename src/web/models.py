6a0c16d0eeb00c1693f5a0027edae27d7b2a97ad
shawnduong <me@shawnd.xyz>
2023-02-17 01:12:37 -0800

    Refactored Account.

--- src/web/models.py
+++ src/web/models.py
@@ -8,90 +8,76 @@ from flask_login import UserMixin
 from typing import Union
 
 class Account(UserMixin, db.Model):
-	"""
-	Users (0) deliberately have lax security to ensure minimal setup and ease
-	of use for non-tech participants. Admins (1) have strict security and are
-	purely web; they do not have cards, and it is impossible to log in as admin
-	using a card ID. Locators (2) are special cards that identify the scanned
-	unit on the admin web interface.
-	"""
 
 	__tablename__ = "accounts"
 
 	id = db.Column(db.Integer, primary_key=True)
 
-	# 0 => user, 1 => admin, 2 => locator
-	acctType = db.Column(db.Integer, unique=False, nullable=False)
-	password = db.Column(db.String(256), unique=False, nullable=True)
+	# type: 0 => user, 1 => admin, 2 => locator
+	type     = db.Column(db.Integer    , unique=False, nullable=False)
+	password = db.Column(db.String(256), unique=False, nullable=True )
+	uid      = db.Column(db.Integer    , unique=True , nullable=True )
+	name     = db.Column(db.String(256), unique=False, nullable=True )
+	email    = db.Column(db.String(256), unique=False, nullable=True )
+	points   = db.Column(db.Integer    , unique=False, nullable=True )
 
-	# cardID is the MFC UID (4 or 7 bytes).
-	cardID  = db.Column(db.Integer    , unique=True , nullable=True)
-	name    = db.Column(db.String(256), unique=False, nullable=True)
-	email   = db.Column(db.String(256), unique=False, nullable=True)
-	points  = db.Column(db.Integer    , unique=False, nullable=True)
+	def __init__(self, type=0, password=None, uid=0, name=None, email=None, points=0):
 
-	def __init__(self, acctType=0, password=None, cardID=0, name=None,
-		email=None, points=0):
-
-		self.acctType  = acctType
+		self.type      = type
 		self.password  = password
-		self.cardID    = cardID
+		self.uid       = uid
 		self.name      = name
 		self.email     = email
 		self.points    = points
 
-		if acctType == 1:
+		if type == 1:
 			self.password = bcrypt.hashpw(password.encode(), bcrypt.gensalt(4))
 
-	def get_user(cardID: str) -> Union[Account, bool]:
+	def get_user(uid: str) -> Union[Account, bool]:
 		"""
-		Check if a cardID string is valid and return the Account if so. Else,
-		return False. This is similar to the login function, except users in
-		Badger log in using a card ID instead of a username/password.
+		Get a user using an 8 or 14 hex char string.
 		"""
 
-		# Convert the cardID string from hex to an integer.
 		try:
-			intCardID = int(cardID, 16)
+			assert (user:=Account.query.filter_by(uid=int(uid, 16)).first()) != None
+			return user
 		except:
 			return False
 
-		if (user:=Account.query.filter_by(cardID=intCardID).first()) != None:
-			return user
-
-		return False
-
 	def login(username: str, password: str) -> Union[Account, bool]:
 		"""
-		This is reserved for the admin. Check if the username/password are
-		valid and return the Account if so. Else, return False.
+		Log into the admin account.
 		"""
 
 		try:
-			if ((user:=Account.query.filter_by(name=username).first()) != None
-				and bcrypt.checkpw(password.encode(), user.password)):
-				return user
+			assert (user:=Account.query.filter_by(name=username).first()) != None
+			assert bcrypt.checkpw(password.encode(), user.password)
+			return user
 		except:
-			pass
-
-		return False
+			return False
 
 	def update_points(self):
 		"""
-		Update the user's points as the sum of their attendances.
+		Update the user's points as attendances+submits-redemptions.
 		"""
 
 		self.points = 0
+
+		submits = Submits.query.filter_by(user=self.id).all()
 		attendances = Attendance.query.filter_by(user=self.id).all()
-		redemptions = CodeRedemption.query.filter_by(user=self.id).all()
+		redemptions = Redemption.query.filter_by(user=self.id).all()
+
+		for submit in submits:
+			code = Code.query.filter_by(id=submit.code).first()
+			self.points += code.points
 
 		for attendance in attendances:
 			event = Event.query.filter_by(id=attendance.event).first()
 			self.points += event.points
 
 		for redemption in redemptions:
-			code = Code.query.filter_by(id=redemption.code).first()
-			self.points += code.value
+			reward = Reward.query.filter_by(id=redemption.reward).first()
+			self.points -= reward.value
 
 		db.session.commit()
 
