930ffc4fc63dc2d1967caeeeb2b9cb6fba74a959
shawnduong <me@shawnd.xyz>
2023-02-17 03:32:04 -0800

    Made form names more dense.

--- src/web/static/js/announcements/create.js
+++ src/web/static/js/announcements/create.js
@@ -1,12 +1,12 @@
 /* Announcement submission. */
-$("#announcement-creation-form").submit(function()
+$("#create-form").submit(function()
 {
 	if (!confirm("Are you sure you want to publish this announcement?"))  return false;
 
 	$.ajax({
 		type: "POST",
 		url: "/api/announcement/create",
-		data: {"timestamp": Date.now(), "content": $("#announcement-creation-content").val()},
+		data: {"timestamp": Date.now(), "content": $("#create-content").val()},
 		success: function()
 		{
 			location.href="/admin/announcements/manage"
@@ -23,11 +23,11 @@ $("#announcement-creation-form").submit(function()
 /* Announcement preview. */
 function update_preview()
 {
-	let content = $("#announcement-creation-content").val();
+	let content = $("#create-content").val();
 	let t = new Date(Date.now());
 	let time = t.toLocaleDateString().slice(0, -5)+" "+
 		t.toLocaleTimeString().slice(0, -6)+t.toLocaleTimeString().slice(-2);
-	$("#announcement-creation-preview-content").html("<p><b>"+time+"</b> "+content+"</p>");
+	$("#preview-content").html("<p><b>"+time+"</b> "+content+"</p>");
 }
 function update_preview_loop()
 {
