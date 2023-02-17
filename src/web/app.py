8d5dea672891298ca7501c5d38139300efb49dff
shawnduong <me@shawnd.xyz>
2023-02-17 01:53:03 -0800

    Moved Flask key to app.

--- src/web/app.py
+++ src/web/app.py
@@ -3,13 +3,11 @@ import sys
 from flask import *
 from flask_sqlalchemy import *
 
-from keys import FLASK_SECRET_KEY
-
 # Instantiate the application and define settings.
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = FLASK_SECRET_KEY
+app.secret_key = "YOUR_KEY_HERE"  # RNG str >16 B should suffice.
 
 # Load the database.
 db = SQLAlchemy(app)
--- src/web/keys.py
+++
@@ -1,2 +0,0 @@
-# A randomly generated string of any length >16 bytes should suffice.
-FLASK_SECRET_KEY = "YOUR_KEY_HERE"
