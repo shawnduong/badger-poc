d06e5b610171734bd320a8901a5859bf7d44aed6
shawnduong <me@shawnd.xyz>
2023-02-19 01:21:21 -0800

    Created example firmware config file.

---
+++ src/firmware/badger/config.h
@@ -0,0 +1,15 @@
+/* Unit unique 64-bit identifier. */
+#define IDENTITY 0x0000000000000000
+
+/* Network details. */
+#define NET_SSID "YOUR_SSID_HERE"
+#define NET_PASS "YOUR_PASS_HERE"
+
+/* How long to wait for a network. */
+#define TIMEOUT 5000  // ms
+
+/* Leave defined in we're in product testing mode. */
+#define QA
+
+/* API endpoint. Omit the trailing slash. */
+#define API "https://YOUR_API_URL_HERE"
